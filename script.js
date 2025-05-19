bdf5069f0805e1486a1e1c3e85643e26cbbac99f
FernandaFaya <fernandafayaf@gmail.com>
2025-05-19 20:45:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let botão = document.querySelector("#botão");
+botão.style.background="purple";
+
+botão.addEventListener("click",e =>{
+    botão.innerHTML="Bem-vindo (a) ao meu site! Fique à vontade para explorar, descobrir e se inspirar. Estou feliz em te ter aqui :).";
+})
+
